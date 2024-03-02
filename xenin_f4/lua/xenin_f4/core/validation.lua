f1dde6347a4ba6ad133502a6c97243acb26559bd
PaleSoda <151110412+PaleSoda@users.noreply.github.com>
2024-03-01 20:48:54 -0600

    Add files via upload

---
+++ xenin_f4/lua/xenin_f4/core/validation.lua
@@ -0,0 +1,7 @@
+XeninUI.Scripts:register("xenin_f4menu", "F4 Menu", "2.5.3", "The Xenin Team", {
+  icon = "Tpm965d",
+  firstParty = true,
+  licensee = "76561198311881522",
+  licenseeProof = "76561198311881522",
+  versionCheck = true
+})
